cace95d1876ae53a0e1768f3f7a9d85974345bee
Dima Chachin <chachin99@bk.ru>
2022-11-30 21:57:15 +0300

    Третье задание

---
+++ Exemple_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+if (num == 1 || num == 2 || num == 3 || num == 4 || num == 5){
+    Console.WriteLine("No");
+}else if (num == 6 || num == 7){
+    Console.WriteLine("Yes");
+} else 
+    Console.WriteLine("Not day a week");
