0eb57fc5fab91fdd1fbde27cf777ac1c90c0b9c4
Dima Chachin <chachin99@bk.ru>
2022-11-30 14:42:15 +0300

    Первое задание

---
+++ Exemple_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ввдите трехзначное число");
+int num1 = int.Parse(Console.ReadLine());
+
+int num2 = num1 / 10;
+int result = num2 % 10;
+
+Console.WriteLine("Среднее число:");
+Console.WriteLine($"{result}");
