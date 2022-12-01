be0dfeb4c63a7a61b96c63da5940382bb579af31
Dima Chachin <chachin99@bk.ru>
2022-12-01 13:02:55 +0300

    Второе задание

---
+++ Exemple_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100){
+    Console.WriteLine($"Число {num} не имеет третей цифры");
+} else {
+
+    Console.Write($"Третье цифра числа {num} равна ");
+
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+
+    int result = num % 10;
+    Console.Write($"{result}");
+}
